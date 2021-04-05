4456df00f0df135f6e03518cb33e8014124c6d6c
matheusvrs <matheus.silva@pixforce.com.br>
2021-04-05 05:38:00 -0300

    adicionei caso de lista vazia

--- pesquisa_sequencial.py
+++ pesquisa_sequencial.py
@@ -3,27 +3,27 @@
     date: april 2021
 '''
 
-def pesquisa_linear(arranjo, item):
+def pesquisa_binaria(arranjo, esquerda, direita, item):
     '''
-       Uma impletação simples em python de uma busca sequencial
-       complexidade: O(n)
+       Uma impletação simples em python de uma busca binária recursiva
+       complexidade: O(logn)
     '''
     if len(arranjo) == 0:
-        return None
-        
-    idx = 0
-    for i in arranjo:
-        if item == i:
-            return idx
-        idx += 1
+        return -1
+    meio = esquerda + direita // 2
     
-    return -1
+    if arranjo[meio] == item:
+        return meio
+    elif arranjo[meio] > item:
+        return pesquisa_binaria(arranjo, meio-1, direita, item)
+    elif arranjo[meio] < item:
+        return pesquisa_binaria(arranjo, esquerda, meio+1, item)
 
 if __name__ in '__main__':
-    arranjo = range(2, 30)
+    arranjo = range(0, 30)
     # ### tests
-    test1 = pesquisa_linear(arranjo, 3)
-    test2 = pesquisa_linear(arranjo, 12)
-    test3 = pesquisa_linear(arranjo, 34)
+    test1 = pesquisa_binaria(arranjo, 0, len(arranjo)-1, 20)
+    test2 = pesquisa_binaria(arranjo, 0, len(arranjo)-1, 2)
+    test3 = pesquisa_binaria(arranjo, 0, len(arranjo)-1, 40)
 
     print(f'test1: {test1} test2: {test2} test3: {test3}')
